e35ee85b836645a14078dfc49deba2082e02e3fb
kietn20 <kietnguyen3698@gmail.com>
2024-09-16 17:23:09 -0700

    finished quicksort in c

---
+++ main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+void swap(int *a, int *b);
+void quick_sort(int *a, int n);
+void quick_sort_helper(int *a, int low, int high);
+int partition(int *a, int low, int high);
+
+int main()
+{
+    int a[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
+    int n = sizeof(a) / sizeof(a[0]);
+    quick_sort(a, n);
+    for (int i = 0; i < n; i++)
+        printf("%d ", a[i]);
+    printf("\n");
+    return 0;
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void quick_sort(int *a, int length)
+{
+    quick_sort_helper(a, 0, length - 1);
+}
+
+void quick_sort_helper(int *a, int low, int high)
+{
+    if (low < high)
+    {
+        int pivot_index = partition(a, low, high);
+        quick_sort_helper(a, low, pivot_index - 1);
+        quick_sort_helper(a, pivot_index + 1, high);
+    }
+}
+
+int partition(int *a, int low, int high)
+{
+    int pivot_value = a[low];
+
+    int i = low + 1;
+
+    for (int j = low + 1; j <= high; j++)
+    {
+        if (a[j] < pivot_value)
+        {
+            swap(&a[i], &a[j]);
+            i++;
+        }
+    }
+    swap(&a[low], &a[i - 1]);
+    return i - 1;
+}
