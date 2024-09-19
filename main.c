508939bf551a88cf7e1da43641d7cbcc6d2d6e6a
kietn20 <kietnguyen3698@gmail.com>
2024-09-19 16:27:17 -0700

    done

--- main.c
+++ main.c
@@ -7,11 +7,14 @@ void quick_sort_helper(int *a, int low, int high);
 int partition(int *a, int low, int high);
 
 // Merge sort algorithm
+void merge_sort(int *a, int n);
+void merge_sort_helper(int *a, int left, int right);
+void merge(int *a, int left, int mid, int right);
 
 int main()
 {
     printf("Quick sort algorithm\n");
-    int a[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
+    int a[] = {38, 27, 43, 3, 9, 82, 10, 4, 6, 7, 12, 15, 18, 20, 22, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100};
     int n = sizeof(a) / sizeof(a[0]);
     quick_sort(a, n);
     for (int i = 0; i < n; i++)
@@ -19,7 +22,13 @@ int main()
     printf("\n");
 
     printf("Merge sort algorithm\n");
-    // ...
+    int b[] = {38, 27, 43, 3, 9, 82, 10, 4, 6, 7, 12, 15, 18, 20, 22, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100};
+    int m = sizeof(b) / sizeof(b[0]);
+    merge_sort(b, m);
+    for (int i = 0; i < m; i++)
+        printf("%d ", b[i]);
+    printf("\n");
+
     return 0;
 }
 
@@ -61,4 +70,69 @@ int partition(int *a, int low, int high)
     }
     swap(&a[low], &a[i - 1]);
     return i - 1;
+}
+
+void merge_sort(int *a, int n)
+{
+    merge_sort_helper(a, 0, n - 1);
+}
+
+void merge_sort_helper(int *a, int left, int right)
+{
+    if (left < right)
+    {
+        int mid = left + (right - left) / 2;
+        merge_sort_helper(a, left, mid);
+        merge_sort_helper(a, mid + 1, right);
+        merge(a, left, mid, right);
+    }
+}
+
+void merge(int *a, int left, int mid, int right)
+{
+    int left_length = mid - left + 1;
+    int right_length = right - mid;
+
+    // temp arrays to store the left and right subarrays
+    int temp_left[left_length], temp_right[right_length];
+
+    // Copy data to temp arrays to be merged
+    for (int i = 0; i < left_length; i++)
+        temp_left[i] = a[left + i];
+    for (int j = 0; j < right_length; j++)
+        temp_right[j] = a[mid + 1 + j];
+
+    // Initial index of temp_left, temp_right, and merged subarrays
+    int i = 0, j = 0, k = left;
+    while (i < left_length && j < right_length)
+    {
+        // determine which element (smallest value) to add to the merged array
+        if (temp_left[i] <= temp_right[j]) //
+        {
+            a[k] = temp_left[i];
+            i++;
+        }
+        else
+        {
+            a[k] = temp_right[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of temp_left array
+    while (i < left_length)
+    {
+        a[k] = temp_left[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of temp_right array
+    while (j < right_length)
+    {
+        a[k] = temp_right[j];
+        j++;
+        k++;
+    }
 }
--- main.hs
+++ main.hs
@@ -1,5 +1,26 @@
+quicksort :: (Ord a) => [a] -> [a]
 quicksort [] = []
 quicksort (x : xs) =
   let smallerSorted = quicksort [a | a <- xs, a <= x]
       biggerSorted = quicksort [a | a <- xs, a > x]
    in smallerSorted ++ [x] ++ biggerSorted
+
+-- helper function to get the first half of a list
+firstHalf :: [a] -> [a]
+firstHalf xs = take (length xs `div` 2) xs
+
+-- helper function to get the second half of a list
+secondHalf :: [a] -> [a]
+secondHalf xs = drop (length xs `div` 2) xs
+
+merge :: (Ord a) => [a] -> [a] -> [a]
+merge xs [] = xs
+merge [] ys = ys
+merge (x : xs) (y : ys)
+  | (x <= y) = x : (merge xs (y : ys)) -- if x is less than or equal to y, then x is the first element of the merged list
+  | otherwise = y : (merge (x : xs) ys) -- if y is less than x, then y is the first element of the merged list
+
+mergesort :: (Ord a) => [a] -> [a]
+mergesort [] = []
+mergesort [x] = [x]
+mergesort xs = merge (mergesort (firstHalf xs)) (mergesort (secondHalf xs))
